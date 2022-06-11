8fa3348709c93765fde5132f080c6ded1d47be1c
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2022-06-11 15:16:59 -0400

    created function to change colors

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,22 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function() {
+   //get random number between 0 - 3
+   //set randomNumber equal to getRandomNumber function
+   const randomNumber = getRandomNumber();
+   console.log(randomNumber);
+
+   //target body and style the backgroundColor set to randomNumber
+   document.body.style.backgroundColor = colors[randomNumber];
+   color.textContent = colors[randomNumber];
+});
+
+//Create a function to generate a random number. 
+//Multiplying by colors.length expands the number past 1 and less than the array
+//Math floor round the number to a whole number
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
